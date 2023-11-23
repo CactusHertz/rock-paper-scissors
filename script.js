b95329ffe365ded4cc500d6e4e77cd815161836d
CactusHertz <50159128+CactusHertz@users.noreply.github.com>
2023-11-23 01:11:52 -0800

    Add playRound() to compute the winner of a game

--- script.js
+++ script.js
@@ -4,3 +4,39 @@ function getComputerChoice(){
     const options = ['rock', 'paper', 'scissors'];
     return options[Math.floor((Math.random() * options.length))]
 }
+
+// compute the winner of a match
+function playRound(playerSelection, computerSelection){
+    playerSelection = playerSelection.toLowerCase();
+    if (playerSelection === computerSelection){
+        return 'You Tie!';
+    }
+    else if (playerSelection === 'rock'){
+        if (computerSelection === 'paper'){
+            return 'You Lose! ' + computerSelection  + ' beats ' + playerSelection;
+        }
+        if (computerSelection === 'scissors'){
+            return 'You Win! ' + playerSelection  + ' beats ' + computerSelection;
+        }
+    }
+    else if (playerSelection === 'paper'){
+        if (computerSelection === 'rock'){
+            return 'You Win! ' + playerSelection  + ' beats ' + computerSelection;
+        }
+        if (computerSelection === 'scissors'){
+            return 'You Lose! ' + computerSelection  + ' beats ' + playerSelection;
+        }
+    }
+    else if (playerSelection === 'scissors'){
+        if (computerSelection === 'rock'){
+            return 'You Lose! ' + computerSelection  + ' beats ' + playerSelection;
+        }
+        if (computerSelection === 'paper'){
+            return 'You Win! ' + playerSelection  + ' beats ' + computerSelection;
+        }
+    }
+}
+
+const playerSelection = getComputerChoice();
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
