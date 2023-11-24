5c80bcb707fd057024c96033e64a31d7a5735f72
CactusHertz <50159128+CactusHertz@users.noreply.github.com>
2023-11-23 19:21:10 -0800

    Add game() to play 5 matches

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ function getComputerChoice(){
     return options[Math.floor((Math.random() * options.length))]
 }
 
-// compute the winner of a match
+// compute the winner of a match 
 function playRound(playerSelection, computerSelection){
     playerSelection = playerSelection.toLowerCase();
     if (playerSelection === computerSelection){
@@ -37,6 +37,32 @@ function playRound(playerSelection, computerSelection){
     }
 }
 
-const playerSelection = getComputerChoice();
-const computerSelection = getComputerChoice();
-console.log(playRound(playerSelection, computerSelection));
+// runs a batch of 5 games
+function game(){
+    let num_games = 5;
+    let player_wins = 0;
+    let computer_wins = 0;
+    for (let x = 0; x < num_games; x++){
+        let playerSelection = prompt('Enter a choice: ');
+        let computerSelection = getComputerChoice();
+        let result = playRound(playerSelection, computerSelection);
+        if(result.charAt(4) === 'W'){
+            player_wins++;
+        }
+        else if (result.charAt(4) === 'L'){
+            computer_wins++;
+        }
+        console.log(result);
+    }
+    if (player_wins === computer_wins){
+        console.log('You Tied with ' + player_wins + ' wins!');
+    }
+    else if (player_wins > computer_wins) {
+        console.log('You Win with ' + player_wins + ' wins!');
+    }
+    else {
+        console.log('You Lost with ' + player_wins + ' wins!');
+    }
+}
+
+game();
